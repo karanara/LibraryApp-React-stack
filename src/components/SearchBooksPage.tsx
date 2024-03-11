101cc3a4ce475faa2dfdb8f0f673d1168531a379
karanara <karanara@mail.uc.edu>
2024-03-11 04:48:36 -0400

    adding page nnumber to search and category

--- src/components/SearchBooksPage.tsx
+++ src/components/SearchBooksPage.tsx
@@ -21,7 +21,8 @@ export const SearchBooksPage = () =>{
           if(searchUrl === ''){
             url = `${baseUrl}?page=${currentPage-1}&size=${booksPerPage}`;
           }else{
-            url = baseUrl + searchUrl
+            let searchWithPage = searchUrl.replace('<pageNumber>',`${currentPage-1}`);
+            url = baseUrl + searchWithPage;
           }
           const response = await fetch(url);
           if(!response.ok){
@@ -70,13 +71,16 @@ export const SearchBooksPage = () =>{
     )
   }
   const searchHandleChange = () =>{
+    setCurrentPage(1);
     if(search === ''){
         setSearchUrl('');
     }else{
-        setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`)
+        setSearchUrl(`/search/findByTitleContaining?title=${search}&page=<pageNumber>&size=${booksPerPage}`)
     }
+    setCategorySelection("Book Category");
   }
   const categoryField = (value:string) =>{
+    setCurrentPage(1);
     if(
         value.toLowerCase() === 'fe' ||
         value.toLowerCase() === 'be' ||
@@ -84,7 +88,7 @@ export const SearchBooksPage = () =>{
         value.toLowerCase() === 'devops'
     ){
         setCategorySelection(value);
-        setSearchUrl(`/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`)
+        setSearchUrl(`/search/findByCategory?category=${value}&page=<pageNumber>&size=${booksPerPage}`)
     }else{
         setCategorySelection("All");
         setSearchUrl(`?page=0&size=${booksPerPage}`)
